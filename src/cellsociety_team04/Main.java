49254d5c923f914bb9659bd9c2d388d0623d3a27
Brian Zhou <blz4@duke.edu>
2016-09-20 09:52:00 -0400

    Created Entrypoint, library image resource folder, and mainMenuinitialization

---
+++ src/cellsociety_team04/Main.java
@@ -0,0 +1,21 @@
+package balloonDodge;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class Main extends Application{
+	Stage stage;
+	
+	public void start(Stage primaryStage){	
+		stage = primaryStage;
+		Scene scene = new Scene(new MainMenu(primaryStage).setUpWindow());
+		primaryStage.setTitle("Cell Society");
+		primaryStage.setScene(scene);;
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

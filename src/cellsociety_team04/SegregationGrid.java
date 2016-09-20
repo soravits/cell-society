7615401e1b83290035caf19d17a1d407866cf1bc
DELIA LI <DL202@duke.edu>
2016-09-20 14:49:25 -0400

    created subclasses for cells and grids in the segregation model and the wa-Tor World model

---
+++ src/cellsociety_team04/SegregationGrid.java
@@ -0,0 +1,19 @@
+package cellsociety_team04;
+
+import javafx.scene.layout.Pane;
+
+public class SegregationGrid extends Grid{
+
+	public SegregationGrid(int rowLength, int sizeOfCell, Pane rootElement,
+			int initialX, int initialY) {
+		super(rowLength, sizeOfCell, rootElement, initialX, initialY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initializeGrid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/cellsociety_team04/WaTorWorldGrid.java
@@ -0,0 +1,19 @@
+package cellsociety_team04;
+
+import javafx.scene.layout.Pane;
+
+public class WaTorWorldGrid extends Grid{
+
+	public WaTorWorldGrid(int rowLength, int sizeOfCell, Pane rootElement,
+			int initialX, int initialY) {
+		super(rowLength, sizeOfCell, rootElement, initialX, initialY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initializeGrid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

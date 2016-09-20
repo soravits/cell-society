cd9f63af3765d1a476f013901df912f10df6dd97
DELIA LI <DL202@duke.edu>
2016-09-20 14:39:29 -0400

    got new files, fixed packaging errors

--- src/cellsociety_team04/Segregation.java
+++ src/cellsociety_team04/Segregation.java
@@ -1,10 +1,31 @@
 package cellsociety_team04;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Segregation extends Simulation{
 
 	public Segregation(int gridLength) {
 		super(gridLength);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startSimulation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopSimulation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Scene init(Stage s) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/cellsociety_team04/WaTorWorld.java
+++ src/cellsociety_team04/WaTorWorld.java
@@ -1,10 +1,31 @@
 package cellsociety_team04;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class WaTorWorld extends Simulation{
 
 	public WaTorWorld(int gridLength) {
 		super(gridLength);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startSimulation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopSimulation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Scene init(Stage s) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }

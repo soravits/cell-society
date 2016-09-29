fb4e03aefe161efc80eba6a6620340037ba2f8f7
DELIA LI <DL202@duke.edu>
2016-09-29 13:05:53 -0400

    created classes needed for user input GUI. A bit of standardizing

---
+++ src/segregation/SegregationInput.java
@@ -0,0 +1,22 @@
+package segregation;
+
+import base.UserInput;
+
+public class SegregationInput extends UserInput{
+	
+	public void selectPercEmpty(){
+		
+	}
+	
+	public void selectPercA(){
+		
+	}
+	
+	public void selectPercB(){
+		
+	}
+	
+	public void selectSatisfyThresh(){
+		
+	}
+}

7519a9069e20edd7da8033c1777348aabe43c157
DELIA LI <DL202@duke.edu>
2016-10-02 00:20:59 -0400

    commenting and brainstorming last part

--- src/xml/SimulationXMLFactory.java
+++ src/xml/SimulationXMLFactory.java
@@ -11,22 +11,32 @@ import javafx.scene.control.Alert.AlertType;
 /**
  * An XMLFactory that gives back a Person object.
  *
- * @author Rhondu Smithwick
+ * @author Rhondu Smithwick, Delia 
  */
 public abstract class SimulationXMLFactory extends XMLFactory {
 
 	private Alert alert;
 	private String defaultVal = " Default value assigned.";
 
+    /**
+     * @param rootElement
+     */
     public SimulationXMLFactory (Element rootElement) {
         super(rootElement);
     }
 
+    /* (non-Javadoc)
+     * @see xml.XMLFactory#isValidFile()
+     */
     @Override
     public boolean isValidFile () {
         return Objects.equals(getAttribute("SimulationType"), getSimulationType());
     }
 
+    /**
+     * Search XML for grid size, account for formatting and other errors
+     * @return gridSize		int value for number of cells on one side of square grid
+     */
     public int getGridSize() {
     	int gridSize = 20;
     	try{
@@ -45,6 +55,11 @@ public abstract class SimulationXMLFactory extends XMLFactory {
         return gridSize;
     }
     
+	/**
+	 * Generates error popup window displaying message for why the value is wrong.
+	 * Notifies user that default value will be used instead
+	 * @param errorText		String describing the error
+	 */
 	public void errorPopup(String errorText) {
 		alert = new Alert(AlertType.ERROR);
 		alert.setTitle("XML Error");
@@ -54,5 +69,9 @@ public abstract class SimulationXMLFactory extends XMLFactory {
 		alert.showAndWait();
 	}
 
+    /**
+     * Looks for which type of simulation it is
+     * @return	String for the name
+     */
     public abstract String getSimulationType();
 }
